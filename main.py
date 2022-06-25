cd1f8cf3307109e9927421dd473f4e739be5e045
Eiko Ujifusa <eikorouji@gmail.com>
2022-06-25 10:42:34 -0700

    working code example

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
 import os
-import chromedriver_autoinstaller
-from selenium import webdriver 
+from selenium import webdriver
 
-os.environ['PATH'] += r"C:/selenium-drivers/chromedriver.exe"
+os.environ['PATH'] += r"C:/SeleniumDrivers"
 driver = webdriver.Chrome()
 driver.get("https://demo.seleniumeasy.com/jquery-download-progress-bar-demo.html")
+driver.implicitly_wait(30)
 my_element = driver.find_element_by_id('downloadButton')
 my_element.click()
-
